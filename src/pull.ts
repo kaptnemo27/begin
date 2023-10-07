80ff9b79d61797821d2852615e4cf2b04af9fb93
M. Zakiyuddin Munziri <belummikir@gmail.com>
2023-10-07 17:39:15 +0800

    feat: set concurrency to 100

--- src/pull.ts
+++ src/pull.ts
@@ -3,6 +3,8 @@ import { db } from "~/drizzle/db";
 import { tableSchema } from "~/drizzle/schema";
 import { Queue, Worker } from "bullmq";
 
+const getCariQueue = new Queue("getCariQueue");
+
 const main = async () => {
   type ListProgramStudi = Awaited<ReturnType<typeof getProgramStudi>>;
   const listProgramStudi: ListProgramStudi = [];
@@ -18,7 +20,6 @@ const main = async () => {
   console.log(`Mencari data...`);
 
   // Create a BullMQ queue for processing getCari tasks
-  const getCariQueue = new Queue("getCariQueue");
 
   await getCariQueue.drain();
 
@@ -40,28 +41,34 @@ const main = async () => {
   }
 
   // Define a worker to process getCari tasks
-  const getCariWorker = new Worker("getCariQueue", async (job) => {
-    const { prodi, index, total } = job.data; //
+  const getCariWorker = new Worker(
+    "getCariQueue",
+    async (job) => {
+      const { prodi, index, total } = job.data; //
 
-    // Fetch data using getCari
-    const data = (await getCari(prodi))["data"];
+      // Fetch data using getCari
+      const data = (await getCari(prodi))["data"];
 
-    console.log(
-      `[${index + 1}/${total}] Fetched ${data.length} data for ${prodi.nama}`,
-    );
+      console.log(
+        `[${index + 1}/${total}] Fetched ${data.length} data for ${prodi.nama}`,
+      );
 
-    if (data.length === 0) return;
+      if (data.length === 0) return;
 
-    if (data.length < 1000) {
-      await db.insert(tableSchema).values(data).onConflictDoNothing();
-      return;
-    }
+      if (data.length < 1000) {
+        db.insert(tableSchema).values(data).onConflictDoNothing();
+        return;
+      }
 
-    for (let i = 0; i < data.length; i += 1000) {
-      const chunk = data.slice(i, i + 1000);
-      await db.insert(tableSchema).values(chunk).onConflictDoNothing();
-    }
-  });
+      for (let i = 0; i < data.length; i += 1000) {
+        const chunk = data.slice(i, i + 1000);
+        db.insert(tableSchema).values(chunk).onConflictDoNothing();
+      }
+    },
+    {
+      concurrency: 100,
+    },
+  );
 
   // Start processing tasks
   await getCariWorker.waitUntilReady();
